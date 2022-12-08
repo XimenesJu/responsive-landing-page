40bbe5a9394fa4c67693ee8b9ec266082725ced5
XimenesJu <ju.cx@hotmail.com>
2022-12-07 22:45:14 -0300

    improving media query css file

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function sizeChanged() {
   if (window.innerWidth <= 768) {
-    menumobile.style.display = 'block'
-  } else menumobile.style.display = 'none'
+    menumobile.style.display = 'none'
+  } else menumobile.style.display = 'block'
 }
 
 
 function clickMenu() {
   if (menumobile.style.display == 'block') {
-    menumobile.style.display = 'block'
+    menumobile.style.display = 'block '
   } else {
     menumobile.style.display = 'none'
   }
