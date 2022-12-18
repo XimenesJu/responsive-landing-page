8439a4d6345cce46f26dcd9a05699c27a84bcc39
XimenesJu <ju.cx@hotmail.com>
2022-12-17 21:47:50 -0300

    getting burguer to show on screen size <= 768 and not to show on screen >= 768, but still not working to open up menu on small screens

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,11 +4,22 @@ function sizeChanged() {
   } else menumobile.style.display = 'block'
 }
 
+function sizeChanged() {
+  if (window.innerWidth <= 768) {
+    burguer.style.display = 'block'
+  } else burguer.style.display = 'none'
+}
 
-// function clickMenu() {
-//   if (menumobile.style.display == 'block') {
-//     menumobile.style.display = 'block '
-//   } else {
-//     menumobile.style.display = 'none'
-//   }
-// }
+function sizeChanged() {
+  if (window.innerWidth >= 768) {
+    burguer.style.display = 'none'
+  } else burguer.style.display = 'block'
+}
+
+function clickMenu() {
+  if (menumobile.style.display == 'block') {
+    menumobile.style.display = 'block '
+  } else {
+    menumobile.style.display = 'none'
+  }
+}
